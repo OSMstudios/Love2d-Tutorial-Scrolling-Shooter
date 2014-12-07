a2cb2584f4e1090b8645b3d5f688d33209af98c0
Dawson Goodell <dawson@westlakedesign.com>
2014-12-06 23:43:23 -0500

    Part 2

--- Part2/main.lua
+++ Part2/main.lua
@@ -2,6 +2,12 @@ debug = true
 
 player = { x = 200, y = 710, speed = 150, img = nil }
 
+-- Timers
+-- We declare these here so we don't have to edit them multiple places
+canShoot = true
+canShootTimerMax = 0.2 
+canShootTimer = canShootTimerMax
+
 -- Image Storage
 bulletImg = nil
 
@@ -11,14 +17,29 @@ bullets = {} -- array of current bullets being drawn and updated
 -- Loading
 function love.load(arg)
 	player.img = love.graphics.newImage('assets/plane.png')
+	bulletImg = love.graphics.newImage('assets/bullet.png')
 end
-
 
 -- Updating
 function love.update(dt)
 	-- I always start with an easy way to exit the game
 	if love.keyboard.isDown('escape') then
 		love.event.push('quit')
+	end
+
+	-- Time out how far apart our shots can be.
+	canShootTimer = canShootTimer - (1 * dt)
+	if canShootTimer < 0 then
+		canShoot = true
+	end
+
+	-- update the positions of bullets
+	for i, bullet in ipairs(bullets) do
+		bullet.y = bullet.y - (250 * dt)
+
+		if bullet.y < 0 then -- remove bullets when they pass off the screen
+			table.remove(bullets, i)
+		end
 	end
 
 	if love.keyboard.isDown('left','a') then
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Love2d-Tutorial-Scrolling-Shooter
 =================================
 
-Source code, images, and builds for the Scrolling Shooter tutorial found here: http://osmstudios.com/tutorials/love2d-in-200-lines
+Source code, images, and builds for the Scrolling Shooter tutorial found here: http://dev.osmstudios.com/tutorials/your-first-love2d-game-in-200-lines-part-2-of-3
