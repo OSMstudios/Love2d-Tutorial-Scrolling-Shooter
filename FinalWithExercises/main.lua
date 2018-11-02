dc087df03f01f09ea3850d651669c93f000fce2b
Par Ahren <par.ahren@infrasec.se>
2018-11-02 11:28:28 +0100

    Do not put newEnemy outside screen

--- FinalWithExercises/main.lua
+++ FinalWithExercises/main.lua
@@ -63,7 +63,7 @@ function love.update(dt)
 		createEnemyTimer = createEnemyTimerMax
 
 		-- Create an enemy
-		randomNumber = math.random(10, love.graphics.getWidth() - 10)
+		randomNumber = math.random(10, love.graphics.getWidth() - 110)
 		newEnemy = { x = randomNumber, y = -10, img = enemyImg }
 		table.insert(enemies, newEnemy)
 	end
